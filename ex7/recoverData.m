1961bf9566f1bf50a0d7d1503a4d3c52fb209035
Jiantao Hu <jh2468@cornell.edu>
2017-03-03 18:07:08 +0900

    7th assignment finished

--- ex7/recoverData.m
+++ ex7/recoverData.m
@@ -9,6 +9,9 @@ function X_rec = recoverData(Z, U, K)
 % You need to return the following variables correctly.
 X_rec = zeros(size(Z, 1), size(U, 1));
 
+Ured = U(:,1:K);
+X_rec = Z*Ured';
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the approximation of the data by projecting back
 %               onto the original space using the top K eigenvectors in U.
